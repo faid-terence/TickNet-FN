099a736746e7446642931ec81a82003167adf615
Terence Faid JABO <faidterence@outlook.com>
2023-12-18 16:08:53 +0200

    ch(CSS): Generate external css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,151 +1,30 @@
+// About.js
 import React from "react";
+import "./About.css";
 
 export const About = () => {
   return (
-    <div
-      style={{ width: "100%", height: "100%", position: "relative", top: 950 }}
-    >
-      <div
-        style={{
-          width: "100%",
-          height: 286,
-          left: 0,
-          top: 0,
-          position: "absolute",
-          background: "#262626",
-        }}
-      />
-      <div
-        style={{
-          width: 99,
-          height: 25,
-          left: 1284,
-          top: 144,
-          position: "absolute",
-          color: "white",
-          fontSize: 20,
-          fontFamily: "Fraunces",
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+    <div className="about-container">
+      <div className="dark-background" />
+      <div className="number-box users">
         100000
         <br />
       </div>
-      <div
-        style={{
-          width: 631,
-          height: 126,
-          left: 139,
-          top: 43,
-          position: "absolute",
-          color: "white",
-          fontSize: 36,
-          fontFamily: "Fraunces",
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+      <div className="headline">
         Streamlining the process of event ticketing
       </div>
-      <div
-        style={{
-          width: 596,
-          height: 61,
-          left: 894,
-          top: 34,
-          position: "absolute",
-          color: "white",
-          fontSize: 20,
-          fontFamily: "Fraunces",
-          fontWeight: "400",
-          wordWrap: "break-word",
-        }}
-      >
+      <div className="description-text">
         At TickNet, we believe that buying tickets for your favourite events
         should be as seamless as possible. We aim to make it smooth.
       </div>
-      <div
-        style={{
-          width: 75,
-          height: 25,
-          left: 906,
-          top: 139,
-          position: "absolute",
-          color: "white",
-          fontSize: 20,
-          fontFamily: "Fraunces",
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+      <div className="number-box events">
         5000
         <br />
       </div>
-      <div
-        style={{
-          width: 75,
-          height: 25,
-          left: 1117,
-          top: 139,
-          position: "absolute",
-          color: "white",
-          fontSize: 20,
-          fontFamily: "Fraunces",
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
-        2000
-      </div>
-      <div
-        style={{
-          width: 110,
-          height: 29,
-          left: 894,
-          top: 179,
-          position: "absolute",
-          color: "white",
-          fontSize: 16,
-          fontFamily: "Fraunces",
-          fontWeight: "400",
-          wordWrap: "break-word",
-        }}
-      >
-        Active Users
-      </div>
-      <div
-        style={{
-          width: 110,
-          height: 29,
-          left: 1100,
-          top: 180,
-          position: "absolute",
-          color: "white",
-          fontSize: 16,
-          fontFamily: "Fraunces",
-          fontWeight: "400",
-          wordWrap: "break-word",
-        }}
-      >
-        Global Events
-      </div>
-      <div
-        style={{
-          width: 110,
-          height: 27,
-          left: 1279,
-          top: 177,
-          position: "absolute",
-          color: "white",
-          fontSize: 16,
-          fontFamily: "Fraunces",
-          fontWeight: "400",
-          wordWrap: "break-word",
-        }}
-      >
-        Tickets Sold
-      </div>
+      <div className="number-box tickets">2000</div>
+      <div className="subheadline active-users">Active Users</div>
+      <div className="subheadline global-events">Global Events</div>
+      <div className="subheadline tickets-sold">Tickets Sold</div>
     </div>
   );
 };
