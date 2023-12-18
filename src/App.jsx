24931a2a24bb192fcea6ddf1d9351b7332cbe94c
Terence Faid JABO <faidterence@outlook.com>
2023-12-18 13:23:04 +0200

    feat(about-section):Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Home } from "./Pages/Home";
-import { Home2 } from "./Pages/Home2";
 
 function App() {
   return (
