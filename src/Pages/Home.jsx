009053f1daccbb5952b8cf3a5cfc4243011bee6b
Terence Faid JABO <faidterence@outlook.com>
2023-12-19 15:21:53 +0200

    feat: Create PaymentOptions component and styling

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Hero } from "../components/Hero/Hero";
 import { About } from "../components/About/About";
+import Card from "../components/About Us/Payment";
 
 export const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <Card />
     </>
   );
 };
