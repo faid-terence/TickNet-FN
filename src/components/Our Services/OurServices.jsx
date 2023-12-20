8bf10acc662101248f16dae513f90ba98ba0a36a
Terence Faid JABO <faidterence@outlook.com>
2023-12-20 15:59:44 +0200

    feat(Services):Implemented Section

---
+++ src/components/Our Services/OurServices.jsx
@@ -0,0 +1,421 @@
+import React from "react";
+import heartIcon from "../../assets/images/Vector.svg";
+import preSaleIcon from "../../assets/images/cart.svg";
+import Vip from "../../assets/images/vip.svg";
+import mobileIcon from "../../assets/images/mobile.svg";
+import moneyIcon from "../../assets/images/money.svg";
+import groupIcon from "../../assets/images/people.svg";
+
+export const OurServices = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        top: "3200px",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: 810,
+          left: 0,
+          top: 0,
+          position: "absolute",
+          background: "#EFEFEF",
+        }}
+      />
+      <div
+        style={{
+          width: 372,
+          height: 224,
+          left: 186,
+          top: 242,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 372,
+            height: 224,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            background: "white",
+            borderRadius: 20,
+          }}
+        />
+        <div
+          style={{
+            width: 120,
+            height: 28,
+            left: 27,
+            top: 72,
+            position: "absolute",
+            color: "black",
+            fontSize: 24,
+            fontFamily: "Fraunces",
+            fontWeight: "700",
+            wordWrap: "break-word",
+          }}
+        >
+          Pre-Sale
+        </div>
+        <div
+          style={{
+            width: 325,
+            height: 38,
+            left: 27,
+            top: 148,
+            position: "absolute",
+            color: "#656565",
+            fontSize: 20,
+            fontFamily: "Fraunces",
+            fontWeight: "400",
+            wordWrap: "break-word",
+          }}
+        >
+          Get exclusive pre-sale tickets for popular events.
+        </div>
+        <div
+          style={{
+            width: 24,
+            height: 30.36,
+            left: 27,
+            top: 25,
+            position: "absolute",
+            // background: "#339657",
+          }}
+        >
+          <img src={preSaleIcon} alt="" />
+        </div>
+      </div>
+      <div
+        style={{
+          width: 372,
+          height: 224,
+          left: 628,
+          top: 242,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 372,
+            height: 224,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            background: "white",
+            borderRadius: 20,
+          }}
+        />
+        <div
+          style={{
+            width: 329,
+            height: 41,
+            left: 25,
+            top: 149,
+            position: "absolute",
+            color: "#656565",
+            fontSize: 20,
+            fontFamily: "Fraunces",
+            fontWeight: "400",
+            wordWrap: "break-word",
+          }}
+        >
+          Secure and easy returns on ticket cancellation.
+        </div>
+        <div
+          style={{
+            width: 212,
+            height: 36,
+            left: 26,
+            top: 76,
+            position: "absolute",
+            color: "black",
+            fontSize: 24,
+            fontFamily: "Fraunces",
+            fontWeight: "700",
+            wordWrap: "break-word",
+          }}
+        >
+          Returns
+        </div>
+        <div
+          style={{
+            width: 25,
+            height: 30.4,
+            left: 25,
+            top: 29,
+            position: "absolute",
+          }}
+        >
+          <img src={heartIcon} alt="" />
+        </div>
+      </div>
+      <div
+        style={{
+          width: 372,
+          height: 224,
+          left: 1102,
+          top: 242,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 372,
+            height: 224,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            background: "white",
+            borderRadius: 20,
+          }}
+        />
+        <div
+          style={{
+            width: 56,
+            height: 29,
+            left: 30,
+            top: 76,
+            position: "absolute",
+            color: "black",
+            fontSize: 24,
+            fontFamily: "Fraunces",
+            fontWeight: "700",
+            wordWrap: "break-word",
+          }}
+        >
+          VIP
+        </div>
+        <div
+          style={{
+            width: 330,
+            height: 47,
+            left: 16,
+            top: 133,
+            position: "absolute",
+            color: "#656565",
+            fontSize: 20,
+            fontFamily: "Fraunces",
+            fontWeight: "400",
+            wordWrap: "break-word",
+          }}
+        >
+          Enjoy premium experiences with our VIP tickets.
+        </div>
+        <div
+          style={{
+            width: 25,
+            height: 34.74,
+            left: 28,
+            top: 24,
+            position: "absolute",
+            // background: "#339657",
+          }}
+        >
+          <img src={Vip} alt="" />
+        </div>
+      </div>
+      <div
+        style={{
+          width: 372,
+          height: 224,
+          left: 189,
+          top: 539,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 372,
+            height: 224,
+            left: -13,
+            top: 0,
+            position: "absolute",
+            background: "white",
+            borderRadius: 20,
+          }}
+        />
+        <div
+          style={{
+            width: 190,
+            height: 30,
+            left: 27,
+            top: 71,
+            position: "absolute",
+            color: "black",
+            fontSize: 24,
+            fontFamily: "Fraunces",
+            fontWeight: "700",
+            wordWrap: "break-word",
+          }}
+        >
+          Mobile Tickets
+        </div>
+        <div
+          style={{
+            width: 15.62,
+            height: 33,
+            left: 27,
+            top: 24,
+            position: "absolute",
+            // background: "#339657",
+          }}
+        >
+          <img src={mobileIcon} alt="" />
+        </div>
+        <div
+          style={{
+            width: 270,
+            height: 50,
+            left: 38,
+            top: 146,
+            position: "absolute",
+            color: "#656565",
+            fontSize: 20,
+            fontFamily: "Fraunces",
+            fontWeight: "400",
+            wordWrap: "break-word",
+          }}
+        >
+          Get your digital ticket on your Smart phone
+        </div>
+      </div>
+      <div
+        style={{
+          width: 372,
+          height: 224,
+          left: 1100,
+          top: 539,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 372,
+            height: 224,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            background: "white",
+            borderRadius: 20,
+          }}
+        />
+        <div
+          style={{
+            width: 257,
+            height: 28,
+            left: 37,
+            top: 58,
+            position: "absolute",
+            color: "black",
+            fontSize: 24,
+            fontFamily: "Fraunces",
+            fontWeight: "700",
+            wordWrap: "break-word",
+          }}
+        >
+          Group Booking
+        </div>
+        <div
+          style={{
+            width: 342,
+            height: 33,
+            left: 30,
+            top: 136,
+            position: "absolute",
+            color: "#656565",
+            fontSize: 20,
+            fontFamily: "Fraunces",
+            fontWeight: "400",
+            wordWrap: "break-word",
+          }}
+        >
+          Book in bulk and enjoy discounted prices.
+        </div>
+        <div
+          style={{
+            width: 25,
+            height: 26.4,
+            left: 37,
+            top: 18,
+            position: "absolute",
+            // background: "#339657",
+          }}
+        >
+          <img src={groupIcon} alt="" />
+        </div>
+      </div>
+      <div
+        style={{
+          width: 372,
+          height: 224,
+          left: 631,
+          top: 539,
+          position: "absolute",
+        }}
+      >
+        <div
+          style={{
+            width: 372,
+            height: 224,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            background: "#339657",
+            borderRadius: 20,
+          }}
+        />
+        <div
+          style={{
+            width: 278,
+            height: 38,
+            left: 29,
+            top: 57,
+            position: "absolute",
+            color: "#F4F4F4",
+            fontSize: 24,
+            fontFamily: "Fraunces",
+            fontWeight: "700",
+            wordWrap: "break-word",
+          }}
+        >
+          E-Tickets
+          <br />
+        </div>
+        <div
+          style={{
+            width: 307,
+            height: 46,
+            left: 29,
+            top: 123,
+            position: "absolute",
+            color: "white",
+            fontSize: 20,
+            fontFamily: "Fraunces",
+            fontWeight: "400",
+            wordWrap: "break-word",
+          }}
+        >
+          Opt for e-ticket for touchless entry.
+        </div>
+        <div
+          style={{
+            width: 36,
+            height: 20,
+            left: 38,
+            top: 24,
+            position: "absolute",
+            // background: "white",
+          }}
+        >
+          <img src={moneyIcon} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
