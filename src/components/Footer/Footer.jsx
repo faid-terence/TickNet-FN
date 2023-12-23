844f213d6f0b74158b211900dd6362f3054ec51d
Terence Faid JABO <faidterence@outlook.com>
2023-12-24 00:24:16 +0200

    feat(testimonials):Add testimonial section to enhance user feedback

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = () => {
         width: "100%",
         height: "100%",
         position: "relative",
-        top: "4560px",
+        top: "600px",
       }}
     >
       <div
